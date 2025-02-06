0b8f706ffb78c3eccbb33ab3d9154af1ba67aaa3
1alibek <alibekgithub@gmail.com>
2025-02-06 13:44:36 +0500

    dome

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Button from './components/button'
 const App = () => {
   return (
     <div>
-      <div>
-        <Button type="primary">Login</Button>
-        <Button type="default">Add</Button>
-        <Button type="danger">Send</Button>
+      <div className='flex ictems-center justify-center gap-4 mt-8'>
+        <Button type="primary">primary</Button>
+        <Button type="default">dafault</Button>
+        <Button type="dashed">dashed</Button>
       </div>
     </div>
   );
--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const Button = ({children,type="primary"}) => {
-      const baseStyle =
-        "bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded-md transition duration-300";
-      const types = {
-        primary: baseStyle,
-        default:
-          "bg-transparent hover:border-1 hover:border-blue-500 !text-black font-normal border-1  border-black !hover:text-blue-500 ",
-        danger:
-          "bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded-md transition duration-300",
-      };
+const Button = ({ children, type = "base" }) => {
+  const baseStyle = " py-2 px-4 rounded-md transition duration-300";
+  const types = {
+    base: baseStyle,
+    primary: "bg-blue-500 hover:bg-blue-400  font-bold text-white",
+
+    default:
+      "bg-transparent hover:border-2 hover:border-blue-500 text-black font-normal border-2  border-black hover:bg-transparent hover:text-blue-500 ",
+    dashed:
+      "bg-transparent hover:border-2 hover:border-blue-500 text-black font-normal border-dashed border-2  border-black hover:bg-transparent hover:text-blue-500 ",
+  };
   return <button className={`1${baseStyle} ${types[type]}`}>{children}</button>;
-}
+};
 
-export default Button
+export default Button;
